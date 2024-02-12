d3ae0b74c41f2cd1b5966bb346e14d917a22ff42
malackapite <veress.marcell@protonmail.com>
2024-02-12 14:44:50 +0100

    8. feladat: +getUresOszlopokSzama, getUresSorokSzama

--- src/modell/Tabla.java
+++ src/modell/Tabla.java
@@ -44,7 +44,7 @@ public class Tabla {
     
     public boolean uresOszlop(int oszlop){
         int ix=0;
-        while(ix <t.length && t[ix][oszlop]!='K') {
+        while(ix <t[0].length && t[ix][oszlop]!='K') {
             ix++;
         }
         return ix<t.length;
@@ -52,9 +52,27 @@ public class Tabla {
     
     public boolean uresSor(int sor){
         int ix=0;
-        while(ix <t[0].length && t[sor][ix]!='K') {
+        while(ix <t.length && t[sor][ix]!='K') {
             ix++;
         }
         return ix<t.length;
+    }
+    
+    public int getUresOszlopokSzama(){
+        int db=0;
+        for (int ix = 0; ix < t[0].length; ix++) {
+            if(uresOszlop(ix))
+                db++;
+        }
+        return db;
+    }
+    
+    public int getUresSorokSzama(){
+        int db=0;
+        for (int ix = 0; ix < t.length; ix++) {
+            if(uresSor(ix))
+                db++;
+        }
+        return db;
     }
 }
